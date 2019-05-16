9442ab097d5bdf5862742c8b7c863b6076480da1
keneifay <42156382+keneifay@users.noreply.github.com>
2019-05-15 23:27:23 -0400

    Initial Script
    Added simple PlayerController

---
+++ Elysia/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D rigidBody;
+    public SpriteRenderer spriteRenderer;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveInput > 0)
+            spriteRenderer.flipX = true;
+        if (moveInput < 0)
+            spriteRenderer.flipX = false;
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rigidBody.velocity = new Vector2(moveInput * speed, rigidBody.velocity.y);
+    }
+}
