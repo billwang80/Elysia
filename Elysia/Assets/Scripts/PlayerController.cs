3aa4dd1239a3e32360ac318751df0727febb1991
BrownDwarf101 <50557613+BrownDwarf101@users.noreply.github.com>
2019-05-16 00:19:11 -0400

    dashtest

--- Elysia/Assets/Scripts/PlayerController.cs
+++ Elysia/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float jumpForce;
     private float moveInput;
+    private float moveInputY;
+    public bool dashing = false; 
 
     private Rigidbody2D rigidBody;
     public SpriteRenderer spriteRenderer;
@@ -38,5 +40,20 @@ public class PlayerController : MonoBehaviour
 
         moveInput = Input.GetAxisRaw("Horizontal");
         rigidBody.velocity = new Vector2(moveInput * speed, rigidBody.velocity.y);
+    }
+    private bool isDashed()
+    {
+        if (!dashing)
+        {
+            moveInput = Input.GetAxisRaw("Horizontal");
+            moveInputY = Input.GetAxisRaw("Vertical");
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                rigidBody.velocity = new Vector2(0, 10);
+                return true; 
+            }
+            return false;
+        }
+        else { return false; }
     }
 }
